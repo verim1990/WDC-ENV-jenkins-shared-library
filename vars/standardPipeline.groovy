37812a344434ee5b7047126efbbe1820ea6337e8
verim1990@gmail.com <vini1990>
2018-11-24 23:11:24 +0100

    Fixes

--- vars/standardPipeline.groovy
+++ vars/standardPipeline.groovy
@@ -5,9 +5,9 @@ def call(body) {
 	body.delegate = config
 	body()
 	
+	env.DOCKER_REVISION = "latest"
 	env.DOCKER_REGISTRY = "${config.docker_registry}"
 	env.DOCKER_IMAGE = "${config.docker_image}"
-	env.DOCKER_REVISION = "latest"
 	
 	node('swarm') {
 
@@ -17,11 +17,15 @@ def call(body) {
 			stage ('Clone') {
 				checkout scm
 			}
+			
 			stage ('Build') {		
-				sh "docker build -t ${env.DOCKER_IMAGE}  ."
+				tag = "${env.DOCKER_IMAGE}:${env.DOCKER_REVISION}"
+				
+				sh "docker build -t ${tag} ."
 
-				app = docker.image(env.DOCKER_IMAGE)
+				app = docker.image(tag)
 			}
+			
 			stage ('Tests') {
 				parallel 'static': {
 					sh "echo 'shell scripts to run static tests...'"
@@ -30,20 +34,35 @@ def call(body) {
 						sh "echo 'static inside test'"
 					}
 				},
+				
 				'unit': {
 					sh "echo 'shell scripts to run unit tests...'"
 				},
+				
 				'integration': {
 					sh "echo 'shell scripts to run integration tests...'"
 				}
 			}
+			
 			stage ('Push image') {
 				docker.withRegistry("${env.DOCKER_REGISTRY}") {
 					app.push("${env.BUILD_NUMBER}")
 					app.push("${env.DOCKER_REVISION}")
 				}
 			}
+			
 			stage ('Deploy') {	
+				switch (env.BRANCH_NAME) {
+					case "staging":					  
+					  break
+
+					case "master":
+					  break
+
+					default:
+					  echo "Skipping deployment, branch ${env.BRANCH_NAME} not configured."
+				  }
+				}
 			}
 		} catch (err) {
 			currentBuild.result = 'FAILED'
